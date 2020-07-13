9e18f56273fca423e73398bf49126831bc2dcec7
ALaDing666 <774332622@qq.com>
2020-07-13 23:33:31 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,92 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+/* 引入vuex */
+/* import Store from './store/store' */
+/* 引入axios */
+import Axios from 'axios'
+/* 引入nprogress 顶部进度条*/
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+/* 引入echarts */
+import echarts from 'echarts'
+
+/* 引入font-aweome */
+import 'font-awesome/css/font-awesome.css'
+
+/* ========= 引入全局组件 ======= */
+import NavMenu from './components/navMenu.vue'
+import HeadMenu from './components/headMenu.vue'
+/* ======= 引入全局组件 结束======= */
+
+/* ======= 引入 ElementUI ======= */
+
+/* 引入css */
+import 'element-ui/lib/theme-chalk/index.css'
+/* 引入组件 */
+import ElementUI from 'element-ui'
+
+/* 引入自定义css文件 */
+import './assets/css/index.css'
+import './assets/css/button.css'
+
+Vue.use(ElementUI)
+    /* ======= 结束 ElementUI ======= */
+
+/* ========= 使用全局组件 ======= */
+Vue.component('nav-menu', NavMenu)
+Vue.component('head-menu', HeadMenu)
+    /* ======= 使用全局组件 结束======= */
+
+/* 引入axios */
+Vue.prototype.$ajax = Axios // vue中使用
+    /* 引入echarts */
+Vue.prototype.$echarts = echarts
+
+Axios.defaults.baseURL = process.env.BASE_URL
+
+/* 跨域访问发送cookie，Axios携带cookie */
+Axios.defaults.withCredentials = true
+
+// 添加请求拦截器，设置axios的loading
+Axios.interceptors.request.use(function(config) {
+    // 请求开始
+    NProgress.start()
+    return config
+})
+
+// 添加响应拦截器
+Axios.interceptors.response.use(function(config) {
+    // 请求结束
+    NProgress.done()
+    return config
+}, function(err) {
+    NProgress.done()
+    return Promise.reject(err)
+})
+
+Vue.config.productionTip = false //阻止启动生产消息
+
+//用户未登录则自动跳转到登录页面
+// router.beforeEach((to, from, next) => {
+//     if (to.path === '/login') {
+//         sessionStorage.removeItem('user')
+//     }
+//     var user = sessionStorage.getItem('user')
+//     if (!user && to.path !== '/login' && to.path !== '/register') {
+//         next({
+//             path: '/login'
+//         })
+//     } else {
+//         next()
+//     }
+// })
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    render: h => h(App)
+})
